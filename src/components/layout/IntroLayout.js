1bf68a2dffeada781c3ed6c62837eca8861b0fcf
Juan Terneus <jterneus@nic.ec>
2020-07-12 19:11:00 -0500

    debugging

--- src/components/layout/IntroLayout.js
+++ src/components/layout/IntroLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IntroMenu from '../introMenu/introMenu';
+import IntroMenu from '../introMenu/IntroMenu';
 
 import '../app.css';
 
